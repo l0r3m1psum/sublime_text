e49b3efac91970dd190b947e816203115e1eec7e
Diego Bellani <bellanidiego@gmail.com>
2025-08-09 09:04:27 +0200

    Added platform layer

--- sublime_text_pdb.py
+++ sublime_text_pdb.py
@@ -10,9 +10,89 @@ import os
 import pdb
 import sys
 
+from typing import Sequence
+
 # subprocess.run is not used because importing the subprocess module changes the
 # behavior of pdb when reaching the end of the program i.e. it starts stepping
 # into the threading module.
+
+def platform_get_from_path(cmd: str) -> str:
+	if os.name == "nt":
+		cmd = cmd.lower()
+
+	paths = os.environ.get("PATH", "").split(os.pathsep)
+
+	if os.name == "nt":
+		exts = os.environ.get("PATHEXT", "").split(os.pathsep)
+	else:
+		exts = ("",)
+	exts = [ext.lower() for ext in exts]
+
+	for path in paths:
+		try:
+			file_list = os.listdir(path)
+		except:
+			file_list = ()
+		if os.name == "nt":
+			file_list = [file.lower() for file in file_list]
+
+		for ext in exts:
+			cmd_file_name = cmd + ext
+			if cmd_file_name in file_list:
+				res = os.path.join(path, cmd)
+				break
+		# Horrible trick to break out of the outer loop if we find the path.
+		else:
+			continue
+		break
+	else:
+		res = ""
+
+	rerurn res
+
+# Adapted from https://github.com/python/cpython/blob/bef9efabc3f899a8f05cc6bee009c7c5fb3d01ae/Lib/subprocess.py#L576
+# https://discuss.python.org/t/how-to-deal-with-unsafe-broken-os-spawn-arg-handling-behavior-on-windows/20829
+def platform_spawn(cmd_path: str, args: Sequence[str]) -> int:
+	if not args:
+		raise ValueError("`args` cannot be empty, the first argument must"
+			" always be the name of the program")
+
+	if os.name == "nt":
+		escaped_args = args[:1]
+		for arg in args[1:]:
+			backslash_counter = 0
+			escaped_arg = []
+			arg_has_space = False
+
+			for char in arg:
+				if char == " " or "\t":
+					arg_has_space = True
+
+				if char == "\\":
+					backslash_counter += 1
+				else:
+					if char == "\"":
+						backslash_counter *= 2
+						char = "\\\""
+					backslashes = "\\" * backslash_counter
+					# Avoid appending empty strings in a loop.
+					if backslashes:
+						escaped_arg.append(backslashes)
+					backslash_counter = 0
+					escaped_arg.append(char)
+
+			backslashes = "\\" * backslash_counter
+			escaped_arg.append("\\"*backslash_counter)
+			arg = ''.join(escaped_arg)
+			needs_quoting = arg_has_space or not arg
+
+			if needs_quoting:
+				arg = "\"" + arg + backslashes + "\""
+			escaped_args.append(arg)
+		args = escaped_args
+
+	res = os.spawnv(os.P_WAIT, cmd_path, args)
+	return res
 
 class SublimeTextPdb(pdb.Pdb):
 	def user_line(self, frame):
@@ -21,54 +101,19 @@ class SublimeTextPdb(pdb.Pdb):
 		if file and self.subl_path:
 			lineno = frame.f_lineno
 			file_and_lineno = "%s:%d" % (file, lineno)
-			exit_code_or_neg_signal = os.spawnv(
-				os.P_WAIT,
+			exit_code_or_neg_signal = platform_spawn(
 				self.subl_path,
 				("subl", "--background", file_and_lineno)
 			)
-			move_cmd = "move_to {\"to\": \"eol\", \"extend\": true}"
-			# https://discuss.python.org/t/how-to-deal-with-unsafe-broken-os-spawn-arg-handling-behavior-on-windows/20829
-			if os.name == "nt":
-				move_cmd = move_cmd.replace("\"", "\\\"")
-				move_cmd = "\"%s\"" % move_cmd
-			# FIXME: sometimes this randomly hangs on Windows.
-			exit_code_or_neg_signal = os.spawnv(
-				os.P_WAIT,
-				self.subl_path,
-				("subl", "--command", move_cmd, "--background")
+			exit_code_or_neg_signal = platform_spawn(
+				elf.subl_path,
+				("subl", "--command", "move_to {\"to\": \"eol\", \"extend\": true}", "--background")
 			)
 
 		super().user_line(frame)
 
 def set_trace(*args, **kwargs):
-	paths = os.environ.get("PATH", "").split(os.pathsep)
-	if os.name == "nt":
-		exts = os.environ.get("PATHEXT", "").split(os.pathsep)
-	else:
-		exts = ("",)
-
-	exts = [ext.lower() for ext in exts]
-
-	for path in paths:
-		try:
-			file_list = os.listdir(path)
-		except:
-			file_list = ()
-
-		if os.name == "nt":
-			file_list = [file.lower() for file in file_list]
-
-		for ext in exts:
-			subl_file_name = "subl" + ext
-			if subl_file_name in file_list:
-				subl_path = os.path.join(path, "subl")
-				break
-		# Horrible trick to break out of the outer loop if we find the path.
-		else:
-			continue
-		break
-	else:
-		subl_path = ""
+	subl_path = platform_get_from_path("subl")
 
 	sublime_text_debugger = SublimeTextPdb(*args, **kwargs)
 	sublime_text_debugger.subl_path = subl_path
